0516422aa19baa197d951c0a5a06300ef5fbda49
Amanda Morgan <AmandaC0022@gmail.com>
2021-07-31 18:57:32 -0500

    add API Url

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,48 @@
 var historyEl = document.getElementById("history"); 
+var searchInputEl = document.getElementById("city-input");  
+var searchBtn = document.getElementById("search-button"); 
+const apiUrl = "api.openweathermap.org"; 
 
+//displays cities from local storage as buttons underneath search 
 function displaySavedLocations() {
-    var locations = ["Chicago", "Washington DC"]; 
-    locations.forEach(function(item) {
-        var listItem = document.createElement("li"); 
-        var content = `<button data-location="${item}">${item}</button>`; 
-        listItem.innerHTML = content; 
-        historyEl.appendChild(listItem); 
-    }); 
+    var locations = localStorage.getItem("savedLocations"); 
+    if (locations) {
+        var parsedLocations = JSON.parse(locations); 
+        parsedLocations.forEach(function(item) {
+            var listItem = document.createElement("li"); 
+            var content = `<button data-location="${item}">${item}</button>`; 
+            listItem.innerHTML = content; 
+            historyEl.appendChild(listItem); 
+        }); 
+    }
 }
 
+//when user clicks on any city button, it will change the content in the current city display 
+function updateContentPane(evt) {
+    var buttonClicked = evt.target; 
+    var location = buttonClicked.getAttribute("data-location"); 
+    window.alert(location); 
+}
+
+function getLocation(evt) {
+    evt.preventDefault(); 
+    var location = searchInputEl.value; 
+    if (!location) {
+        window.alert("Please enter a location"); 
+    } 
+}
+
+//creates Event Listeners on all of the cities 
+function setEventListeners() {
+    historyEl.addEventListener("click", updateContentPane); 
+    searchBtn.addEventListener("click", getLocation); 
+}
+
+//beginning function 
 function init() {
+    setEventListeners()
     displaySavedLocations(); 
+
 }
 
 init(); 
